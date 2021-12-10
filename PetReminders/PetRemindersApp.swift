1714a4c54f4efebf8cda6481db8cbdeaf84b578f
Jacob Gil <jacobgil325@gmail.com>
2021-12-10 18:24:48 -0500

    Initial Commit

---
+++ PetReminders/PetRemindersApp.swift
@@ -0,0 +1,17 @@
+//
+//  PetRemindersApp.swift
+//  PetReminders
+//
+//  Created by Jacob Gil on 12/10/21.
+//
+
+import SwiftUI
+
+@main
+struct PetRemindersApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
